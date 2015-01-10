a0680b5b4eae3e2a271e68d85727a8df54eab1a2
Kelvin Tay <kelvintaywl@gmail.com>
2015-01-11 02:13:24 +0900

    [chore] add sturcturing for angular

---
+++ ui/public/javascripts/angular/main.js
@@ -0,0 +1,8 @@
+(function(){
+
+	var resturants = [];
+
+	// angular app
+	angular.module('snakebite-ui', ['restaurant'])
+	.constant('SERVER_URL', 'http://localhost:8000');
+})();
---
+++ ui/public/javascripts/angular/restaurant.js
@@ -0,0 +1,58 @@
+angular.module('restaurant', [
+
+])
+.controller('restaurantListCtrl', ['$http', '$scope', 'SERVER_URL', function($http, $scope, serverURL){
+	$scope.restaurants = [
+		{
+			id: 1,
+			name: 'KFC',
+			address: '2-25-6 Asakusa, Taito-ku, Tokyo',
+			email: 'info@kfc.co.jp',
+			tags: ['chicken', 'fried', 'xmas'],
+			menus: [
+				{
+					name: 'XMAS bucket',
+					price: 800,
+					currency: 'JPY',
+					tags: ['xmas', 'family'],
+					images: ['test image url']
+				}
+			]
+		},
+		{
+			id: 2,
+			name: 'KFC',
+			address: '2-25-6 Asakusa, Taito-ku, Tokyo',
+			email: 'info@kfc.co.jp',
+			tags: ['chicken', 'fried', 'xmas'],
+			menus: [
+				{
+					name: 'XMAS bucket',
+					price: 800,
+					currency: 'JPY',
+					tags: ['xmas', 'family'],
+					images: ['test image url']
+				},
+				{
+					name: 'XMAS bucket2',
+					price: 800,
+					currency: 'JPY',
+					tags: ['xmas', 'family'],
+					images: ['test image url']
+				}
+			]
+		}
+	];
+
+	var req = {
+		method: 'GET',
+		url: serverURL + '/restaurants',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	$http(req).success(function(data){
+			$scope.restaurants = data.items;
+	});
+}]);
