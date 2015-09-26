1326db99dab5877c94388e8c11c41a6a7a24d5dc
Kelvin Tay <kelvintaywl@gmail.com>
2015-09-27 00:05:13 +0900

    [feature] add ACL permission check in auth middleware

--- snakebite/middlewares/auth.py
+++ snakebite/middlewares/auth.py
@@ -2,7 +2,40 @@
 from __future__ import absolute_import
 import jwt
 from snakebite.libs.error import HTTPUnauthorized
+from snakebite.services.user import get_user
+from snakebite.models.user import Role
 from snakebite import constants
+
+# role-based permission control
+ACL_MAP = {
+    '/menus': {
+        'get': Role.USER,
+        'post': Role.OWNER
+    },
+    '/menus/+': {
+        'get': Role.USER,
+        'put': Role.OWNER,
+        'delete': Role.OWNER
+    },
+    '/restaurants': {
+        'get': Role.USER,
+        'post': Role.EMPLOYEE
+    },
+    '/restaurants/+': {
+        'get': Role.USER,
+        'put': Role.OWNER,
+        'delete': Role.OWNER
+    },
+    '/users': {
+        'get': Role.EMPLOYEE,
+        'post': Role.EMPLOYEE,
+    },
+    '/users/+': {
+        'get': Role.EMPLOYEE,
+        'put': Role.EMPLOYEE,
+        'delete': Role.EMPLOYEE
+    }
+}
 
 
 class JWTAuthMiddleware(object):
@@ -11,9 +44,24 @@ class JWTAuthMiddleware(object):
     def __init__(self, secret):
         self._secret = secret
 
-    def _is_user_authorized(self, req, user_id, acl):
-        # TODO implement check of acl against user
-        return True
+    def _access_allowed(self, req, user):
+        method = req.method.lower() or 'get'
+        path = req.path.lower()
+
+        if path not in ACL_MAP:
+            # try replacing :id value with `+`
+            sub_path, _, id = path.rpartition('/')
+            if not sub_path:
+                return False  # unable to find a logical path for ACL checking
+            path = "{}/+".format(sub_path)
+            if path not in ACL_MAP:
+                return False
+
+        return user.role_satisfy(ACL_MAP[path].get(method, Role.USER))  # defaults to minimal role if method not found
+
+    def _is_user_authorized(self, req, user_id):
+        user = get_user(user_id)
+        return user is not None and self._access_allowed(req, user)
 
     def process_request(self, req, res):
 
@@ -40,10 +88,10 @@ class JWTAuthMiddleware(object):
             )
 
         user_id = decoded.pop("sub")
-        acl = decoded.pop("acl")
+        decoded.pop("acl")
 
         # check if user is authorized to this request
-        if not self._is_user_authorized(req, user_id, acl):
+        if not self._is_user_authorized(req, user_id):
             raise HTTPUnauthorized(
                 title='Authorization Failed',
                 description='User does not have privilege/permission to view requested resource.'
